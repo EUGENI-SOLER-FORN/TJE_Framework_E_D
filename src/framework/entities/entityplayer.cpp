bdc16ca2de961c4368708359b26a7e6ff87534e9
davidobrero <114682777+davidobrero@users.noreply.github.com>
2024-05-25 18:01:59 +0200

    EntityPlayer

--- src/framework/entities/entityplayer.cpp
+++ src/framework/entities/entityplayer.cpp
@@ -7,7 +7,7 @@ void EntityPlayer::update(float seconds_elapsed)
 	const Vector3& player_front = this->model.frontVector().normalize();
 	const Vector3& player_right = this->model.rightVector().normalize();
 
-	//Code adapted from Camera controls given in the framework skeleton
+	// Code adapted from Camera controls given in the framework skeleton
 	// Run with shift
 	if (Input::isKeyPressed(SDL_SCANCODE_LSHIFT)) moving_speed *= 10;
 	
@@ -16,5 +16,53 @@ void EntityPlayer::update(float seconds_elapsed)
 	if (Input::isKeyPressed(SDL_SCANCODE_A) || Input::isKeyPressed(SDL_SCANCODE_LEFT)) this->model.translate( - player_right * moving_speed);
 	if (Input::isKeyPressed(SDL_SCANCODE_D) || Input::isKeyPressed(SDL_SCANCODE_RIGHT)) this->model.translate(player_right * moving_speed);
 
-	
+	//TODO: Manage all related with collisions
 }
+
+void EntityPlayer::render(Camera* camera)
+{
+	
+	// Render mesh
+	EntityMesh::render(camera);
+
+	// Load shaders and mesh, get model matrix and enable shader
+	Shader* shader = Shader::Get("basic.vs", "flat.fs");
+	Mesh* mesh = Mesh::Get("data/meshes/sphere.obj");
+	Matrix44 m = model;
+	shader->enable();
+
+	// Do translation and scaling transformations
+	m.translate(0.0f, player_height, 0.0f);
+	m.scale(sphere_radius, sphere_radius, sphere_radius);
+
+	// Set basic uniforms values
+	shader->setUniform("u_color", Vector4(0.0f, 1.0f, 0.0f, 1.0f));
+	shader->setUniform("u_viewprojection", camera->viewprojection_matrix);
+	shader->setUniform("u_model", m);
+
+	// Render mesh
+	mesh->render(GL_LINES);
+
+	// Disable shader
+	shader->disable();
+
+	//TODO: Try to render sphere for collisions
+}
+
+/*bool checkPlayerCollisions(const Vector3& target_pos,
+	std::vector<sCollisionData>& collisions) {
+	Vector3 center = target_pos + Vector3(0.f, 1.25f, 0.f);
+	float sphereRadius = 0.75f;
+	Vector3 colPoint, colNormal;
+	// For each collider entity “e” in root:
+	Mesh* mesh = e->mesh;
+	if (mesh->testSphereCollision(e->model, center,
+		sphereRadius, colPoint, colNormal)) {
+		collisions.push_back({ colPoint,
+		colNormal.normalize() });
+	}
+	// End loop
+	return !collisions.empty();
+}*/
+
+
--- src/framework/entities/entityplayer.h
+++ src/framework/entities/entityplayer.h
@@ -4,9 +4,20 @@
 
 class EntityPlayer : public EntityMesh {
 public:
+
+	struct sCollisionData {
+		Vector3 colPoint;
+		Vector3 colNormal;
+	};
+
+	// constructor methods
 	EntityPlayer() : EntityMesh() { this->name = "player"; this->model = Matrix44::IDENTITY; };
 	~EntityPlayer() {};
 	
+	// general attributes
+	float player_height = 1.0f;
+	float sphere_radius = 1.0f;
+
 	// stats of the player
 	float player_speed = 0.01f;
 	float player_hunger = 0.f;
@@ -15,7 +26,14 @@ public:
 	// helper method
 	Vector3 position() { return this->model.getTranslation(); };
 
+	// methods to manage hunger and sleep
+	void hunger() { this->player_hunger = std::min(100.0f, this->player_hunger + 50.0f); }
+	void sleep() { this->player_sleepiness = std::min(100.0f, this->player_sleepiness + 50.0f); }
+
+	// update and render methods
 	void update(float seconds_elapsed) override;
-	// TODO
 	void render(Camera* camera) override {};
+
+	// methods to manage collisions
+	// bool checkPlayerCollisions(const Vector3& target_pos, std::vector<sCollisionData>& collisions);
 };
